38517c3ad5e0e674bd341fd3d38d7207e1de261b
Morten Spjøtvoll <morten.spjotvoll@hansencx.com>
2023-04-14 13:31:15 +0200

    Initial commit

---
+++ TagCloud/TagCloudSetting.cs
@@ -0,0 +1,44 @@
+﻿using LemmaSharp;
+using System.Text.RegularExpressions;
+
+namespace TagCloud;
+
+/// <summary>
+/// Represents the settings for a TagCloudAnalyzer instance.
+/// </summary>
+public class TagCloudSetting
+{
+    private static Regex defaultWordFinder = new Regex(@"[\w\']+", RegexOptions.Compiled);
+    private static LemmatizerPrebuilt defaultLemmatizer = new LemmatizerPrebuiltFull(LanguagePrebuilt.English);
+
+    /// <summary>
+    /// Initializes a new instance of the TagCloudSetting class. 
+    /// </summary>
+    public TagCloudSetting()
+    {
+        this.WordFinder = defaultWordFinder;
+        this.Lemmatizer = defaultLemmatizer;
+        this.MaxCloudSize = 100;
+        this.NumCategories = 10;
+    }
+
+    /// <summary>
+    /// Gets or sets the regex used to find words within strings.
+    /// </summary>
+    public Regex WordFinder { get; set; }
+
+    /// <summary>
+    /// Gets or sets the lemmatizer used to derive the roots of words.
+    /// </summary>
+    public LemmatizerPrebuilt Lemmatizer { get; set; }
+
+    /// <summary>
+    /// Gets or sets the maximum size of the tag cloud.
+    /// </summary>
+    public int MaxCloudSize { get; set; }
+
+    /// <summary>
+    /// Gets or sets the number of tag categories.
+    /// </summary>
+    public int NumCategories { get; set; }
+}
